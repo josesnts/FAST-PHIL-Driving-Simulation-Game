6fb3c2caa91977733ed9e8a1bf851636a97da2da
josesnts <santosjoseantoniorogel@gmail.com>
2024-06-03 16:37:07 +0800

    First Commit

---
+++ mainGame/fastphildsg_game/tdc/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../functions/header.php'; ?>
+</head>
+
+<body class="overflow-hidden">
+
+    <div class="bg-image d-flex align-items-center justify-content-center flex-column"
+                    style="
+                        background-image: url('../images/displayBackground.svg');
+                        height: 100vh;
+                        width: 1560px;
+                        background-repeat: no-repeat;
+                        background-size: cover;
+                    "
+    >
+       
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
